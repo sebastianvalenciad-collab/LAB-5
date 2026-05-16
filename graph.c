d8f4ccd638091080146973bc6d4789a1a3d339c2
Your Name <you@example.com>
2026-05-16 04:34:48 +0000

    new try

--- graph.c
+++ graph.c
@@ -28,10 +28,8 @@ int is_equal_string(void *key1, void *key2) {
 
 Graph* createGraph() 
 {
-    Graph* grafo = (Graph*) malloc(sizeof(Graph));
-    if(grafo == NULL) return NULL;
-    grafo->adjacencyMap = map_create(is_equal_string);
-    return grafo;
+
+    return NULL;
 }
 
 void addNode(Graph* g, const char* label) {
